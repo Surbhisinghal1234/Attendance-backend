44d35a0c387659fbc4d82f385c301bd2bd91c870
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-06-07 18:39:58 +0530

    add attendance Api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const port = 3000;
@@ -24,6 +24,7 @@ const password = encodeURIComponent(process.env.MONGO_PASSWORD);
 // );
 // if (dbConnection)
 //   app.listen(port, () => console.log("Server Started on port " + port));
+//..............................................................................
 
 mongoose.connect(
     "mongodb+srv://" +
@@ -53,39 +54,47 @@ mongoose.connect(
 let d = new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" });
 
 const attendanceSchema = new mongoose.Schema({
-  date: {
-    type: Date,
-    default: d,
-  },
-  attendance: {
-    type: Object,
-    required: true,
-  },
+    date: {
+        type: Date,
+        default: d,
+    },
+    faculty: {
+        type: String,
+        required: true,
+    },
+    attendance: {
+        type: Object,
+        required: true,
+    },
 });
 const facultySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  id: {
-    type: Number,
-    default: Date.now(),
-  },
+    name: {
+        type: String,
+        required: true,
+    },
+    id: {
+        type: Number,
+        default: Date.now(),
+    },
 });
 
 const studentSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    default: Date.now(),
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  faculty: {
-    type: String,
-    required: true,
-  },
+    id: {
+        type: Number,
+        default: Date.now(),
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    faculty: {
+        type: String,
+        required: true,
+    },
+    aadharCard: { 
+        type: String,
+        required: true,
+    }
 });
 
 const attendanceModel = mongoose.model("record", attendanceSchema, "record");
@@ -93,73 +102,93 @@ const facultyModel = mongoose.model("faculty", facultySchema, "faculty");
 const studentModel = mongoose.model("student", studentSchema, "student");
 
 app.post("/saveAttendance", (req, res) => {
-  const dataToSave = new attendanceModel(req.body);
-  dataToSave
-    .save()
-    .then((response) => res.send(response))
-    .catch((error) => {
-      console.log(error);
-      res.send(false);
-    });
+    const dataToSave = new attendanceModel(req.body);
+    dataToSave
+        .save()
+        .then((response) => res.send(response))
+        .catch((error) => {
+            console.log(error);
+            res.send(false);
+        });
 });
 
 app.get("/getFaculty", async (req, res) => {
-  const facultyList = await facultyModel.find({}).select("-__v");
-  if (facultyList) res.json(facultyList);
-  else res.json(false);
+    const facultyList = await facultyModel.find({}).select("-__v");
+    if (facultyList) res.json(facultyList);
+    else res.json(false);
 });
 
 app.post("/saveFaculty", async (req, res) => {
-  const dataToSave = new facultyModel(req.body);
-  dataToSave
-    .save()
-    .then((response) => res.json(response))
-    .catch((error) => {
-      console.log(error);
-      res.send(false);
-    });
+    const dataToSave = new facultyModel(req.body);
+    console.log(dataToSave, "dataToSave")
+    dataToSave
+        .save()
+        .then((response) => {
+            console.log("Save response:", response);
+            res.json(response)
+        })
+        .catch((error) => {
+            console.log("error", error);
+            res.send(false);
+        });
 });
 
+
 app.get("/getStudent", async (req, res) => {
-  const studentList = await studentModel.find({}).select("-__v");
-  if (studentList) res.json(studentList);
-  else res.json(false);
+    const { faculty } = req.query;
+    try {
+        let studentList;
+        if (faculty) {
+            studentList = await studentModel.find({ faculty }).select("-__v");
+        } else {
+            studentList = await studentModel.find({}).select("-__v");
+        }
+        res.json(studentList);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "error" });
+    }
 });
 
 app.post("/saveStudent", async (req, res) => {
-  const dataToSave = new studentModel(req.body);
-  dataToSave
-    .save()
-    .then((response) => res.json(response))
-    .catch((error) => {
-      console.log(error);
-      res.send(false);
-      console.log( "dataToSave",dataToSave);
-    });
+    // const { name, faculty, aadhaarCard } = req.body
+    const dataToSave = new studentModel(req.body);
+
+    dataToSave
+        .save()
+        .then((response) => res.json(response))
+        .catch((error) => {
+            console.log(error);
+            res.send(false);
+            console.log("dataToSave", dataToSave);
+
+        });
 });
 
-app.delete("/deleteStudent/:id", async (req, res) =>{
-  const idToDelete = req.params.id;
-  const deletedStudent = await studentModel.findOneAndDelete({
-    id: idToDelete
-  });
-  if (deletedStudent) res.json("Student Deleted");
-  else res.json(false);
+app.delete("/deleteStudent/:id", async (req, res) => {
+    const idToDelete = req.params.id;
+    const deletedStudent = await studentModel.findOneAndDelete({
+        id: idToDelete
+    });
+    if (deletedStudent) res.json("Student Deleted");
+    else res.json(false);
 });
 
 app.delete("/deleteFaculty/:id", async (req, res) => {
     const idToDelete = req.params.id;
     try {
-      const deletedFaculty = await facultyModel.findOneAndDelete({ _id: idToDelete });
-      if (deletedFaculty) {
-        res.json("Faculty Deleted");
-      } else {
-        res.json(false);
-      }
+        const deletedFaculty = await facultyModel.findOneAndDelete({ _id: idToDelete });
+        if (deletedFaculty) {
+            res.json("Faculty Deleted");
+        } else {
+            res.json(false);
+        }
     } catch (error) {
-      console.error("Error", error);
-      res.json(false);
+        console.log("error", error);
+        res.json(false);
     }
-  });
-  
+});
+
+
+
 
